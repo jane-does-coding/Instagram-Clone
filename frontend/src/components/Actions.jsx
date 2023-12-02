b8a495ed078ca54382735c5e2d658100d47e1407
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2023-12-02 09:21:10 -0700

    poster footer is done

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,5 +1,41 @@
+import { Box, Flex } from "@chakra-ui/react";
+import {
+  CommentLogo,
+  NotificationsLogo,
+  UnlikeLogo,
+} from "../assets/constants";
+import { PiShareFat } from "react-icons/pi";
+import { useState } from "react";
+
 const Actions = () => {
-  return <>hey</>;
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(234);
+
+  const handleLike = () => {
+    if (liked) {
+      setLikes(likes - 1);
+    } else {
+      setLikes(likes + 1);
+    }
+
+    setLiked(!liked);
+  };
+
+  return (
+    <>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2}>
+        <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
+          {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
+        </Box>
+        <Box cursor={"pointer"} fontSize={18}>
+          <CommentLogo />
+        </Box>
+        <Box cursor={"pointer"} fontSize={18}>
+          <PiShareFat size={28} />
+        </Box>
+      </Flex>
+    </>
+  );
 };
 
 export default Actions;
--- frontend/src/components/FeedPosts/PostFooter.jsx
+++ frontend/src/components/FeedPosts/PostFooter.jsx
@@ -15,34 +15,18 @@ import {
 } from "../../assets/constants";
 import { GoPaperAirplane } from "react-icons/go";
 import { PiShareFat } from "react-icons/pi";
+import Actions from "../Actions";
 
 const PostFooter = ({ title, isProfilePage }) => {
-  const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(234);
-
-  const handleLike = () => {
-    if (liked) {
-      setLikes(likes - 1);
-    } else {
-      setLikes(likes + 1);
-    }
-
-    setLiked(!liked);
-  };
 
   return (
     <Box>
-      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2}>
-        <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
-          {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
-        </Box>
-        <Box cursor={"pointer"} fontSize={18}>
-          <CommentLogo />
-        </Box>
-        <Box cursor={"pointer"} fontSize={18}>
-          <PiShareFat size={28} />
-        </Box>
-      </Flex>
+      <Text fontSize={"sm"} fontWeight={500} my={2}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+        numquam.
+      </Text>
+      <Actions />
       <Flex
         fontWeight={600}
         fontSize={"sm"}
@@ -55,9 +39,6 @@ const PostFooter = ({ title, isProfilePage }) => {
         <Box borderRadius={"5px"} bg={"gray.500"} h={"3px"} w={"3px"} />
         636 comments
       </Flex>
-      <Text fontSize={"sm"} fontWeight={500} mt={2}>
-        {title}
-      </Text>
 
       {/* Input */}
 
