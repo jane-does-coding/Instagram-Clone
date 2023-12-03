05d869a34b366c3fd1f89c654c363872ace5814c
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2023-12-03 10:57:22 -0700

    loading animation halfway done

--- frontend/src/components/FeedPosts/FeedPosts.jsx
+++ frontend/src/components/FeedPosts/FeedPosts.jsx
@@ -1,34 +1,62 @@
-import { Container } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Skeleton,
+  SkeletonCircle,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import FeedPost from "./FeedPost";
+import { useState } from "react";
 
 const FeedPosts = () => {
+  const { isLoading, setIsLoading } = useState(true);
+  const posts = [];
+
   return (
-    <Container maxW={"container.sm"}>
+    <>
+      {/* LOADING */}
+      {[0, 1, 2].map((_, idx) => (
+        <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+          <Flex gap="2">
+            <SkeletonCircle size="10" />
+            <VStack gap={2} alignItems={"flex-start"}>
+              <Skeleton height="10px" w={"200px"} />
+              <Skeleton height="10px" w={"200px"} />
+            </VStack>
+          </Flex>
+          <Skeleton w={"full"}>
+            <Box h={"400px"}>contents wrapped</Box>
+          </Skeleton>
+        </VStack>
+      ))}
+      {/* POSTS */}
       <FeedPost
         img="/img1.png"
-        username="newuser_237"
+        title="First post"
         avatar="/img1.png"
-        title="Wonderful image"
+        username="janedoe_846"
       />
       <FeedPost
         img="/img2.png"
-        username="newuser_7477"
+        title="Second post"
         avatar="/img2.png"
-        title="This is my first post"
+        username="johndoe_103"
       />
       <FeedPost
         img="/img3.png"
-        username="newuser_103"
+        title="Third post"
         avatar="/img3.png"
-        title="Hello World"
+        username="burak_935"
       />
       <FeedPost
         img="/img4.png"
-        username="newuser_037"
+        title="Fourth post"
         avatar="/img4.png"
-        title="This is a great post"
+        username="anotheruser_026"
       />
-    </Container>
+    </>
   );
 };
 
