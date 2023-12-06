f1e9f70bc221d407695f1ccae567095a1d383914
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2023-12-05 19:31:19 -0700

    User model done

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`Server started at the port ${PORT}`);
