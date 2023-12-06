e1dba9f0a76e85b8a5cfa88b82962756ae457888
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2023-12-05 20:47:48 -0700

    Post model done

---
+++ backend/models/postModel.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      maxLength: 1000,
+    },
+    image: {
+      type: String,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    saved: {
+      type: Number,
+      default: 0,
+    },
+    replies: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+        userProfilePic: {
+          type: String,
+        },
+        username: {
+          type: String,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -42,3 +42,7 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
