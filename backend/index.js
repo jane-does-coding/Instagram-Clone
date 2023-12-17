d88d717d7a45d046020d04f6ac93f478a811f0ef
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2023-12-17 16:27:00 -0700

    minor bug fixed in profileHeader component

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,14 @@ import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
